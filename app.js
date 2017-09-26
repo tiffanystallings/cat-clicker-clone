f4d28d04d38fbaed0e16d08ba9ba23063f2307d9
Tiffany <tiffany.c.stallings@gmail.com>
2017-09-26 17:18:55 -0400

    Add navbar to pick a cat to click.

--- app.js
+++ app.js
@@ -27,26 +27,49 @@ var cats = [
 ];
 
 $(document).ready(function() {
-	var catDiv = document.createElement("div");
-	var newP = document.createElement("p");
 
-	for (i=0; i < cats.length; i++) {
+	for (i=0; i<cats.length; i++) {
 		console.log('adding a cat...')
-		$('section').append('<div class="cat"></div>');
+		$('.links').append('<li class="cat-link">' + cats[i].name + '</li>');
 
-		var catSelector = 'section .cat:nth-child(' + (i + 1) + ')';
-		$(catSelector).css('background-image', 'url(' + cats[i].image + ')');
-		$(catSelector).append('<p class="count">0</p>');
-		$(catSelector).append('<p class="name">' + cats[i].name + '</p>');
+		//var catSelector = 'section .cat:nth-child(' + (i + 1) + ')';
+		//$(catSelector).css('background-image', 'url(' + cats[i].image + ')');
+		//$(catSelector).append('<p class="count">0</p>');
+		//$(catSelector).append('<p class="name">' + cats[i].name + '</p>');
 	}
-	$('.cat').click(function(e) {
+	$('.cat-link').click(function() {
+		var catName = $(this).text()
+		var cat = searchCats(catName);
+		$('.cat').css('background-image', 'url(' + cat.image + ')');
+		$('.count').text(cat.clicks);
+		$('.name').text(cat.name);
+	});
+
+	$('.cat').click(function() {
 		incrementCount($(this));
 	});
 });
 
+function searchCats(name) {
+	for (i=0; i<cats.length; i++) {
+		if (name == cats[i].name) {
+			return cats[i];
+		}
+	}
+	return null;
+}
+
 function incrementCount(e) {
-	var $count = e.find('.count')
-	var current = parseInt($count.text(),10);
-	current ++;
-	$count.text(current);
+	var name = e.find('.name').text()
+	var cat = searchCats(name);
+	var count = e.find('.count')
+	//var current = parseInt(count.text(),10);
+	cat.clicks ++;
+	count.text(cat.clicks);
+
+	//for (i=0; i<cats.length; i++) {
+	//	if (name == cats[i].name)
+	//}
+	//current ++;
+	//count.text(current);
 }
