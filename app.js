77777f01a5f82184ee338638234de90da6261327
Tiffany <tiffany.c.stallings@gmail.com>
2017-09-26 15:46:24 -0400

    Add ability to handle multiple cats

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+var cats = [
+	{
+		'name': 'Ser Meow',
+		'image': 'sermeow.jpg'
+	},
+	{
+		'name': 'Pounce',
+    	'image': 'pounce.jpg'
+    }
+];
 
 $(document).ready(function() {
-	$('#cat').click(function(e) {
-		incrementCount();
+	var catDiv = document.createElement("div");
+	var newP = document.createElement("p");
+
+	for (i=0; i < cats.length; i++) {
+		console.log('adding a cat...')
+		$('section').append('<div class="cat"></div>');
+
+		var catSelector = 'section .cat:nth-child(' + (i + 1) + ')';
+		$(catSelector).css('background-image', 'url(' + cats[i].image + ')');
+		$(catSelector).append('<p class="count">0</p>');
+		$(catSelector).append('<p class="name">' + cats[i].name + '</p>');
+	}
+	$('.cat').click(function(e) {
+		incrementCount($(this));
 	});
 });
 
-function incrementCount() {
-	var current = parseInt($('#count').text(), 10);
+function incrementCount(e) {
+	var $count = e.find('.count')
+	var current = parseInt($count.text(),10);
 	current ++;
-	$('#count').text(current);
+	$count.text(current);
 }
